ce0a437d94c0ff56c4834ebd1285041767622048
samnaniaahil <37119335+samnaniaahil@users.noreply.github.com>
2019-12-24 12:44:45 -0500

    Update fraud_detection.py

--- fraud_detection.py
+++ fraud_detection.py
@@ -1,4 +1,4 @@
-# Fraud Detection in Banks with SOMs
+# Fraud Detection in Banks with SOMs and ANNS
 
 # Part 1 - Creating a list of frauds with a Self-Organizing Map (SOM)
 
@@ -8,10 +8,10 @@ import matplotlib.pyplot as plt
 import pandas as pd
 
 
-# Import the dataset
+# Importing the dataset
 dataset = pd.read_csv(r'C:\Users\firoj\Downloads\P16-Self-Organizing-Maps\Self_Organizing_Maps\Credit_Card_Applications.csv')
 
-# Seperate dataset into dependent and independent variable(s)
+# Seperating dataset into dependent and independent variable(s)
 X = dataset.iloc[:, :-1].values
 y = dataset.iloc[:, -1].values
 
@@ -21,7 +21,7 @@ from sklearn.preprocessing import MinMaxScaler
 sc = MinMaxScaler(feature_range = (0, 1))
 X = sc.fit_transform(X)
 
-# Train the SOM
+# Training the SOM
 from minisom import MiniSom
 # Initialize parameters 
 som = MiniSom(x = 10, y = 10, input_len = 15, sigma = 1.0, learning_rate = 0.5)
@@ -48,7 +48,7 @@ for i, x in enumerate(X):
     plot(w[0] + 0.5, w[1] + 0.5, markers[y[i]], markeredgecolor = colors[y[i]], markerfacecolor = 'None', markersize = 10, markeredgewidth = 2)
 show()
 
-# Get a list of frauds
+# Getting a list of frauds
 mappings = som.win_map(X)
 # Get coordinates of outlying winning nodes
 frauds = np.concatenate((mappings[(5,3)], mappings[(8,3)]), axis = 0)
@@ -75,7 +75,7 @@ sc = StandardScaler()
 customers = sc.fit_transform(customers)
 
 
-# Part 3 - Creating an ANN to create a ranked list of determine probability of each customer having committed fraud
+# Part 3 - Creating an ANN to determine how likely a customer is to have committed fraud
 
 # Importing Keras libraries and packages
 import keras
@@ -109,5 +109,7 @@ y_pred = classifier.predict(customers)
 # Create 2-d array of of customer IDs and predicted probabilites
 y_pred = np.concatenate((dataset.iloc[:, 0:1].values, y_pred), axis = 1)
 
-# Sort customers from least to most likely of having committed fraud
+
+# Part 4 - Sorting customers from least to most likely of having committed fraud
+
 y_pred = y_pred[y_pred[:, 1].argsort()]
